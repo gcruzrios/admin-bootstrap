048fe59ab06c4aa27bc9179bf75bf303b28ec29b
gcruzrios <gcruzr@gmail.com>
2023-08-05 13:11:43 -0600

    Index v0.1

---
+++ src/pages/Index.js
@@ -0,0 +1,23 @@
+
+import React, { useState } from "react";
+import {
+    BrowserRouter as Router,
+    Route,
+    Redirect,
+    Link,
+  } from "react-router-dom";
+
+import Header from "../components/Header.js";
+import Footer from "../components/Footer.js";
+import Content from "../components/Content.js";
+import Sidebar from "../components/Sidebar";
+const Index = () => {
+  return (
+    <div>
+        <Header/>    
+
+    </div>
+  )
+}
+
+export default Index
